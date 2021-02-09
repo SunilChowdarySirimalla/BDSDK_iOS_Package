cafaee0d20a5bf731621d64cc0c9dab339cddd54
Sunil Chowdary <ss079m@att.com>
2021-02-08 22:09:45 -0600

    Updated Library Name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "BDSDK_iOS",
+    name: "BrightDiagnostics",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BDSDK_iOS",
-            targets: ["BDSDK_iOS"]),
+            name: "BrightDiagnostics",
+            targets: ["BrightDiagnostics"]),
     ],
     dependencies: [
             // Dependencies declare other packages that this package depends on.
             // .package(url: /* package url */, from: "1.0.0"),
         ],
         targets: [
-            .binaryTarget(name: "BDSDK_iOS", path: "./Sources/BDSDK_iOS/BrightDiagnostics.xcframework")
+            .binaryTarget(name: "BrightDiagnostics", path: "./Sources/BDSDK_iOS/BrightDiagnostics.xcframework")
             ]
 )
