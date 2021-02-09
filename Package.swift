fd81d703f06eb8dfc2fe5e8ca89b76ffd7a8b8fa
Sunil Chowdary <ss079m@att.com>
2021-02-08 23:24:35 -0600

    updated package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "BrightDiagnosticsPackage",
+    name: "BrightDiagnostics",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BrightDiagnosticsPackage",
+            name: "BrightDiagnostics",
             targets: ["BrightDiagnostics"]),
     ],
     dependencies: [
