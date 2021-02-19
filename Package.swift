33bc3bf64826ae06ee6699932071458647198133
Sunil Chowdary <ss079m@att.com>
2021-02-19 12:08:10 -0600

    Removed minimum deployement target

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "BrightDiagnosticsPackage",
-    platforms: [
-        .iOS(.v12),
-        .tvOS(.v12),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
