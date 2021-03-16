d5a5177ab82e83a29e9e261a2a79cb4e82d5a01e
Sunil Chowdary <ss079m@att.com>
2021-03-16 08:06:02 -0500

    Updated platforms

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
     name: "BrightDiagnostics",
     platforms: [
-        .iOS(.v11),
-        .tvOS(.v11),
+            .iOS(.v11),
+            .macOS(.v10_14),
+            .tvOS(.v10),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
